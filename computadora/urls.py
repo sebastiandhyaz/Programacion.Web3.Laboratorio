ade917acaacf06627c6fbe637465fdce901f1e10
Sebastián Díaz <93691660+sebastiandhyaz@users.noreply.github.com>
2025-05-08 00:29:44 +0000

    delete archivos

--- computadora/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('lista_computadoras/', views.listado_computadoras, name='listado_computadoras'),
-]
--- computadora/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from .models import Computadora
-
-def listado_computadoras(request):
-    computadoras = Computadora.objects.all()
-    return render(request, 'computadora/listado_computadoras.html', {'computadoras': computadoras})
-
